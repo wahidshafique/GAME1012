57f314e9b8aa2aa7224918f4aa90246a870d3b71
Wahid Shafique <wahidshafique@hotmail.com>
2015-01-27 19:09:23 -0500

    bad code

---
+++ Lab2Project/Lab2Project/Assets/MyAssets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float speed=0.1f;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (DestroyAfterDelay (3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (new Vector3 (0, 0, this.speed))}
+		private IEnumerator DestroyAfterDelay (float delay){
+		yield return new WaitForSeconds (delay);
+		GameObject.Destroy (this.gameObject);
+		}	                        
+
+}
---
+++ Lab2Project/Lab2Project/Assets/MyAssets/Scripts/Turret.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	public Rotation bodyRotation=null;
+	public Rotation gunRotation=null;
+	public float rotationSpeed=1;
+	private Rotation turrentRotation=null;
+	public Transform spawner=null;
+
+
+	void awake(){
+		this.turrentRotation = this.gameObject.GetComponent<Rotation> ();
+		}
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		CheckInput();
+	}
+
+	private void CheckInput(){
+
+		RotateBodyInput();
+		RotateGunInput ();
+		Move ();
+		RotateTurret ();
+		FireInput ();
+	}
+	private void FireInput(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject projectile = Instantiate (this.projectile  Prefab) as GameObject;
+			projectile.transform.position=this.spawner.postion;
+			projectile.transform.position=this.spawner.postion;
+				}
+		}
+	private void RotateTurret(){
+				if (Input.GetKey (KeyCode.A)) {
+						this.turrentRotation.Rotate (-this.rotationSpeed);
+				}
+				if (Input.GetKey (KeyCode.D)) {
+			this.turrentRotation.Rotate (this.rotationSpeed);
+				}
+		}
+	private void Move(){
+				if (Input.GetKey (KeyCode.W)) {
+						this.transform.Translate (Vector3.forward);
+				}
+				if (Input.GetKey (KeyCode.S)) {
+						this.transform.Translate (Vector3.back);
+				}
+		}
+
+	private void RotateBodyInput(){
+		if (Input.GetKey (KeyCode.RightArrow)){
+			this.bodyRotation.Rotate(this.rotationSpeed);
+	}
+		if (Input.GetKey (KeyCode.LeftArrow)){
+			this.bodyRotation.Rotate(-this.rotationSpeed);
+		}
+	}
+	private void RotateGunInput (){
+		if (Input.GetKey (KeyCode.UpArrow)){
+			this.gunRotation.Rotate(-this.rotationSpeed);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			this.gunRotation.Rotate(-this.rotationSpeed);
+		}
+}
+		}
