4508dee3c7efee06fb1d2c746113620f9bed8bb1
Wahid Shafique <wahidshafique@hotmail.com>
2015-03-19 00:12:45 -0400

    finalized

--- Lab7Project/Lab7Project/Assets/MyAssets/Scripts/Level/Switch.cs
+++ Lab7Project/Lab7Project/Assets/MyAssets/Scripts/Level/Switch.cs
@@ -5,13 +5,24 @@ public class Switch : MonoBehaviour
 {
 	[SerializeField] private GameObject barrier;
 
+	void Awake () {
+		barrier.GetComponent<ParticleSystem>().maxParticles = 0;
+	}
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.tag == "Player")
 		{
 			AudioManager.Instance.PlaySwitchClip();
-			GameObject.Destroy(barrier);
+			StartCoroutine (destroybarr());
+
 		}
 	}
-
+IEnumerator destroybarr () {
+	barrier.GetComponent<ParticleSystem>().maxParticles = 1000;
+	yield return new WaitForSeconds (1f);
+	GameObject.Destroy(barrier);
 }
+}
+
+
