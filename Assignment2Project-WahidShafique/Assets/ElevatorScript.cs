9c809d53173b4f5ccf979b9f1a0a24057d2850b2
Wahid Shafique <wahidshafique@hotmail.com>
2015-03-27 01:30:56 -0400

    Add assignment 2

---
+++ Assignment2Project-WahidShafique/Assets/ElevatorScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorScript : MonoBehaviour {
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Fire"){
+			Destroy(this.gameObject);
+			explosion.SetActive(true);
+		}
+
+	}
+}
