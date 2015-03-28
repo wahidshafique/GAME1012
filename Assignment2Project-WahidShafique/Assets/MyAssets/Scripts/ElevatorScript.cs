90ef8973e4c1e9b2d449b2862ac24bd096d523f7
Wahid Shafique <wahidshafique@hotmail.com>
2015-03-27 23:46:35 -0400

    added assignment 2 changes
    level changes

---
+++ Assignment2Project-WahidShafique/Assets/MyAssets/Scripts/ElevatorScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorScript : MonoBehaviour {
+	public GameObject explosion;//object to be activated when platform goes away
+	private bool platActive = false; //platform starts inactive
+
+	void Update () {
+		if (platActive)//moves down if active 
+		transform.Translate(Vector3.down * Time.deltaTime, Space.World);//moves platform down in world space 
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Fire"){//trigger explosion and destroy when plat in near fire 
+			Destroy(this.gameObject);
+			explosion.SetActive(true);
+		}
+		StartCoroutine (moveDown());//delay a bit once player in on plat
+	}
+	IEnumerator moveDown (){
+		yield return new WaitForSeconds (0.5f);
+		platActive = true; //once player is in trigger, sets the plat to active
+	}
+}
---
+++ Assignment2Project-WahidShafique/Assets/RobotBoyControlScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotBoyControlScript : MonoBehaviour {
+	//controls robot boy and subsequent interaction
+	Animator anim;
+	new Rigidbody2D rigidbody2D;
+	
+	private bool facingRight=true; //Robot boy always faces right when made
+	private bool grounded = false; //Robot boy starts a few inches above ground
+	private bool moveCheck = true; //checks if you are moving
+	private float groundRad = 0.2f;
+	private float move;
+	private float maximumSpeed = 2f; 
+	private float jumpForce = 150f;
+
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	public AudioClip jumpClip;
+
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	void FixedUpdate () { //walking 
+		move = Input.GetAxis ("Horizontal");
+		
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRad, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+		
+		anim.SetFloat ("VSpeed", rigidbody2D.velocity.y);
+		
+		if (moveCheck) {
+			anim.SetFloat("Speed", Mathf.Abs(move));
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maximumSpeed,rigidbody2D.velocity.y);
+			if (move > 0 && !facingRight)
+				Flipper();
+			else if (move<0 && facingRight)
+				Flipper();
+		
+		if ( grounded && Input.GetKeyDown(KeyCode.Space)){ //jumping 
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+			this.GetComponent<AudioSource>().Play();
+			
+		}
+	}
+}
+	
+	void Flipper (){
+		
+		facingRight = !facingRight;
+		
+		Vector3 TheScale = transform.localScale;
+		
+		TheScale.x *= -1;
+		
+		transform.localScale = TheScale;
+	}	
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.tag == "Fire"){
+			//Destroy(this.gameObject);
+			anim.SetBool("Death", true);
+			moveCheck = false; //now the player cannot move with arrows
+			//this.rigidbody2D.isKinematic = true;
+		}
+	}
+}
+
